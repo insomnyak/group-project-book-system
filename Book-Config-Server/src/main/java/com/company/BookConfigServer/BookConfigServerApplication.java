a5c060095bcfd0f71e33a23d631696dee2f06dbe
insomnyak <rene@insomnyak.com>
2019-09-18 14:46:23 -0400

    first commit

---
+++ Book-Config-Server/src/main/java/com/company/BookConfigServer/BookConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.company.BookConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class BookConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookConfigServerApplication.class, args);
+	}
+
+}
