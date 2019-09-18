08c9441105f1acf44338dee18bfb49a3a6ec87d2
insomnyak <rene@insomnyak.com>
2019-09-18 14:57:19 -0400

    removing unnecessary config files, adding book-service

--- book-service/src/test/java/com/company/bookservice/BookServiceApplicationTests.java
+++ book-service/src/test/java/com/company/bookservice/BookServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.trilogyed.springconfigserver;
+package com.company.bookservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringConfigServerApplicationTests {
+public class BookServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
