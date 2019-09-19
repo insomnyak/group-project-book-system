82b4f0fa4d7e9bab6b5ecddde45e60e55f1ee050
insomnyak <rene@insomnyak.com>
2019-09-18 22:15:50 -0400

    new attempt to add modified notes without .DS_Store

---
+++ note-service/src/main/java/com/company/noteservice/controller/NoteServiceController.java
@@ -0,0 +1,12 @@
+package com.company.noteservice.controller;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class NoteServiceController {
+
+
+
+}
